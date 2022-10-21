defabebb90ec7c5c16e9a895eb8cda3c8781fbaa
Julian Benegas <julianbenegas99@gmail.com>
2022-10-21 08:43:55 -0700

    tweak readme to include the suppressHydrationWarning for the countdown server-client  mismatch (#25)

--- examples/nextjs-shopify/src/pages/_app.tsx
+++ examples/nextjs-shopify/src/pages/_app.tsx
@@ -9,6 +9,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <CountdownProvider
       endDate={Date.now() + 1000 * 5} // set this to 5 seconds from now just to test
       countdownChildren={<Countdown />}
+      exitDelay={1000} // optional, just to give some time to animate the countdown before finally unmounting it
+      startDate={Date.now()} // optional, just if you need some kind of progress UI
     >
       <QueryClientProvider>
         <Component {...pageProps} />
